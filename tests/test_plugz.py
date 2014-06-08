320af46f473105abe3f3f944d402a876bd499756
Matti Gruener <gruener.matti@gmail.com>
2014-06-08 21:06:49 +0200

    add files after failed merge

--- tests/test_plugz.py
+++ tests/test_plugz.py
@@ -7,11 +7,12 @@ test_plugz
 
 Tests for `plugz` module.
 """
+import os
 
 import pytest
+
 import plugz
-from plugz import errors
-from plugz.pluginbase import PluginTypeBase
+from plugz import errors, PluginTypeBase, StandardPluginType
 
 def test_loading_plugins_without_correct_arguments():
     """ Loading Plugins without giving valid arguments should fail.
@@ -43,3 +44,30 @@ def test_loading_plugins_without_correct_arguments():
     # if paths are given but one is invalid, raise the error
     with pytest.raises(errors.InvalidPluginPath):
         plugins = plugz.load_plugins(['/invalid/path/' ], MyValidType)
+
+
+def test_loading_plugins_for_standard_plugin_type():
+    """    """
+    rel_path_tokens = ['data', 'plugins', 'standard', 'valid']
+    valid_dir = os.path.join(os.path.dirname(__file__), *rel_path_tokens)
+    plugins = plugz.load_plugins([valid_dir], StandardPluginType)
+
+    assert len(plugins) == 2, \
+           'Test did not find the two valid plugins in data. Found %s' % plugins
+
+    # make sure the loaded plugins have the correct plugintype.
+    assert all(map(lambda x: x.plugintype == StandardPluginType.plugintype, plugins))
+    # make sure it's the correct plugins that were loaded.
+    assert 'MyFirstValidPlugin' in [p.__name__ for p in plugins]
+    assert 'MyValidPluginTwo' in [p.__name__ for p in plugins]
+
+    for p in plugins:
+        instance = p()
+        instance.activate()
+        # print 'Activated:', instance
+
+    rel_path_tokens = ['data', 'plugins', 'standard', 'invalid']
+    invalid_dir = os.path.join(os.path.dirname(__file__), *rel_path_tokens)
+    plugins = plugz.load_plugins([invalid_dir], StandardPluginType)
+
+    assert len(plugins) == 0 # they are all invalid
