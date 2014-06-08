ad78061be833b07fd33b08d2dc9999308f38a789
Matti Gruener <gruener.matti@gmail.com>
2014-06-08 19:23:30 +0200

    add some basic test plugins that can be used to verify the correct reaction to valid or invalid plugins

---
+++ tests/data/plugins/standard/valid/plugin_one.py
@@ -0,0 +1,16 @@
+import plugz
+
+@plugz.register_plugin
+class MyValidPluginOne(plugz.StandardPluginType):
+    """ Basic plugin for tests.
+
+    Inherit from StandardPluginType and implement activate().
+    """
+
+    def __init__(self, ):
+        """ """
+        pass
+
+    def activate(self):
+        """ """
+        pass
---
+++ tests/data/plugins/standard/valid/plugin_two.py
@@ -0,0 +1,17 @@
+import plugz
+
+@plugz.register_plugin
+class MyValidPluginTwo(plugz.StandardPluginType):
+    """ Basic plugin for tests.
+
+    Inherit from StandardPluginType and implement activate().
+    """
+
+    def __init__(self, ):
+        """
+        """
+        pass
+
+    def activate(self):
+        """ """
+        pass
