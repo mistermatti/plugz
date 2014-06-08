0d3750128e2c8363d48d68c1ca95fe7b0b7a9086
Matti Gruener <gruener.matti@gmail.com>
2014-06-08 20:42:12 +0200

    add description of how to create custom types

--- plugz/plugintypes/standard_plugin_type.py
+++ plugz/plugintypes/standard_plugin_type.py
@@ -3,15 +3,18 @@ from abc import abstractmethod
 from plugz import PluginTypeBase
 
 class StandardPluginType(PluginTypeBase):
-    """ Simple plugin type that requires an activate() method. """
+    """ Simple plugin type that requires an activate() method.
 
-    plugintype = 'StandardPluginType'
+    Plugins that derive from this type will have to implement
+    activate() or plugz will refuse to load the plugin.
 
-    def __init__(self, ):
-        """ """
-        super(StandardPluginType).__init__(self)
+    The plugintype needs to be set as well. Set it to a string
+    that describes the class. It does not have to be the class
+    name but it might make things easier for you.
+
+    """
+
+    plugintype = __name__
 
     @abstractmethod
-    def activate(self):
-        """ """
-        raise NotImplementedError, self.__class__
+    def activate(self): pass
