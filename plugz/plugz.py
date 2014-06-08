e3fcba6a8d2ebfb100fa971698681ed62197f548
Matti Gruener <gruener.matti@gmail.com>
2014-06-08 20:42:54 +0200

    add plugintype attr and remove obsolete init

--- plugz/plugz.py
+++ plugz/plugz.py
@@ -4,10 +4,9 @@ class PluginTypeBase(object):
 
     __metaclass__ = abc.ABCMeta
 
+    plugintype = None
+
     @staticmethod
     def is_valid_file(file):
         """ """
         return file.endswith('.py')
-
-    def __init__(self ):
-        pass
